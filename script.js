72f6e9aa7cb0eba69f23e114f40eddd4b29e09fa
Ro5hi <slen.detablan@gmail.com>
2022-03-20 12:23:44 -0600

    testing rotation and translation of canvas

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ window.addEventListener('load', function(){
     canvas.height = window.innerHeight * 0.8;
 
     // Canvas Settings
-        ctx.filLStyle = 'green';
+        ctx.fillStyle = 'green';
         ctx.strokeStyle = 'yellow';
         ctx.lineWidth = 30;
         ctx.lineCap = 'round';
+
         
-    // Effect Settings
+        // Effect Settings
         let size = 200;
-        
+
+        ctx.translate(100,100);
+        ctx.rotate(0.3);
+        ctx.fillRect(0,0,canvas.height, canvas.width);
         ctx.beginPath();
         ctx.moveTo(canvas.width/2,canvas.height/2);
         ctx.lineTo(size,canvas.height/2);
