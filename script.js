65cb9d836390a79cd9d3ca5ce077559a8ede0325
Ro5hi <slen.detablan@gmail.com>
2022-03-20 13:18:12 -0600

    added shadows for greater detail of fractal

--- script.js
+++ script.js
@@ -9,17 +9,26 @@ window.addEventListener('load', function(){
         ctx.strokeStyle = 'orange';
         ctx.lineWidth = 10;
         ctx.lineCap = 'round';
+        ctx.shadowColor = 'rgba(0,0,0,0.7)';
+        ctx.shaddowOffsetX = 10;
+        ctx.shaddowOffsetY = 5;
+        ctx.shadowBlur = 10;
 
 
     // Effect Settings
         let size = 200;
-        let sides = 8;
+        let sides = 5;
 
         // Max Level determines the dept of the fractal
         let maxLevel = 3;
+        // Scale Determines size of the segments
         let scale = 0.4;
+        // Spread determines angle or radians from the parent branch
         let spread = 0.6;
+        // Branches determines number of branches created within the fractal
         let branches = 6;
+
+    
 
     // Draw Branches
         function drawBranch(level){
