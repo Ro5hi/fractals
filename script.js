a66377a5a64fc752523013ccd5e00ad29977b545
Ro5hi <slen.detablan@gmail.com>
2022-03-20 12:14:55 -0600

    added starter script

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  })
